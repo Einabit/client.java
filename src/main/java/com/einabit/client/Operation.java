6c813f676e68b18e3de5d2e356e0c01105ca8955
Nikolay Mokhrin Kuralesin <nicomness@gmail.com>
2023-05-29 16:40:28 +0100

    Add javadoc

--- src/main/java/com/einabit/client/Operation.java
+++ src/main/java/com/einabit/client/Operation.java
@@ -1,9 +1,29 @@
 package com.einabit.client;
 
+/**
+ * Available operations in Einabit services
+ */
 public enum Operation {
 
+    /**
+     * Value operation.
+     * <p>
+     * Used to obtain the current value of a variable.
+     */
     VALUE,
+
+    /**
+     * Fetch operation.
+     * <p>
+     * Used to obtain all the values of a variable between two dates.
+     */
     FETCH,
+
+    /**
+     * Tap operation.
+     * <p>
+     * Operation which allows you to subscribe to a variable and continuously receive its values.
+     */
     TAP
 
 }
